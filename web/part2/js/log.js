dfd17b4de564e92e33f2a28726293dcae53171c9
Yustynn Panicker <yustynn@gmail.com>
2019-04-29 04:51:19 +0800

    Add basic, unstyled results

--- web/part2/js/log.js
+++ web/part2/js/log.js
@@ -1,3 +1,5 @@
+histories = []
+
 function resetHistory() {
 	history = [];
 }
@@ -10,20 +12,24 @@ function addHistory(position, rewardType) {
 
 function logHistory() {
 	let string = JSON.stringify(history);
+	histories.push(history);
 	let log = {
 		rounds,
 		history: string,
 	};
 
-	let body = JSON.stringify(log);
-	fetch('http://10.12.1.221:5000/log', {
-		method: 'POST',
-		mode: 'no-cors',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body,
-	});
+	// let body = JSON.stringify(log);
+	// fetch('http://10.12.1.221:5000/log', {
+	// 	method: 'POST',
+	// 	mode: 'no-cors',
+	// 	headers: {
+	// 		'Content-Type': 'application/json',
+	// 	},
+	// 	body,
+	// });
 
 	rounds += 1;
+	console.log(rounds)
+
+	if (rounds > 5) loadResults();
 }
